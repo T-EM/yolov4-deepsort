bca05cecdef5675bd3950d7efd62e31f81719a1b
T-EM <tanmay.e.mahale@gmail.com>
2022-09-18 00:44:28 +0530

    V

--- object_tracker.py
+++ object_tracker.py
@@ -1,54 +1,57 @@
+from tools import generate_detections as gdet
+from deep_sort.tracker import Tracker
+from deep_sort.detection import Detection
+from deep_sort import preprocessing, nn_matching
+from tensorflow.compat.v1 import InteractiveSession
+from tensorflow.compat.v1 import ConfigProto
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+from PIL import Image
+from core.config import cfg
+from tensorflow.python.saved_model import tag_constants
+from core.yolov4 import filter_boxes
+import core.utils as utils
+from absl.flags import FLAGS
+from absl import app, flags, logging
+import tensorflow as tf
+import time
 import os
 # comment out below line to enable tensorflow logging outputs
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import time
-import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
-from absl import app, flags, logging
-from absl.flags import FLAGS
-import core.utils as utils
-from core.yolov4 import filter_boxes
-from tensorflow.python.saved_model import tag_constants
-from core.config import cfg
-from PIL import Image
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
 # deep sort imports
-from deep_sort import preprocessing, nn_matching
-from deep_sort.detection import Detection
-from deep_sort.tracker import Tracker
-from tools import generate_detections as gdet
-flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
-flags.DEFINE_string('weights', './checkpoints/yolov4-416',
+flags.DEFINE_string('framework', 'tflite', '(tf, tflite, trt')
+flags.DEFINE_string('weights', './checkpoints/yolov4-416-tiny.tflite',
                     'path to weights file')
 flags.DEFINE_integer('size', 416, 'resize images to')
-flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
+flags.DEFINE_boolean('tiny', True, 'yolo or yolo-tiny')
 flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
-flags.DEFINE_string('video', './data/video/test.mp4', 'path to input video or set to 0 for webcam')
+flags.DEFINE_string('video', "2", 'path to input video or set to 0 for webcam')
 flags.DEFINE_string('output', None, 'path to output video')
-flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
+flags.DEFINE_string('output_format', 'XVID',
+                    'codec used in VideoWriter when saving video to file')
 flags.DEFINE_float('iou', 0.45, 'iou threshold')
 flags.DEFINE_float('score', 0.50, 'score threshold')
 flags.DEFINE_boolean('dont_show', False, 'dont show video output')
 flags.DEFINE_boolean('info', False, 'show detailed info of tracked objects')
 flags.DEFINE_boolean('count', False, 'count objects being tracked on screen')
 
+
 def main(_argv):
     # Definition of the parameters
     max_cosine_distance = 0.4
     nn_budget = None
     nms_max_overlap = 1.0
-    
+
     # initialize deep sort
     model_filename = 'model_data/mars-small128.pb'
     encoder = gdet.create_box_encoder(model_filename, batch_size=1)
     # calculate cosine distance metric
-    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    metric = nn_matching.NearestNeighborDistanceMetric(
+        "cosine", max_cosine_distance, nn_budget)
     # initialize tracker
     tracker = Tracker(metric)
 
@@ -70,7 +73,8 @@ def main(_argv):
         print(output_details)
     # otherwise load standard tensorflow saved model
     else:
-        saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
+        saved_model_loaded = tf.saved_model.load(
+            FLAGS.weights, tags=[tag_constants.SERVING])
         infer = saved_model_loaded.signatures['serving_default']
 
     # begin video capture
@@ -90,8 +94,18 @@ def main(_argv):
         codec = cv2.VideoWriter_fourcc(*FLAGS.output_format)
         out = cv2.VideoWriter(FLAGS.output, codec, fps, (width, height))
 
+    ############################# Face Detection ##############################
+    labels = ["Tanmay"]
+
+    face_cascade = cv2.CascadeClassifier(
+        './data/haarcascade_frontalface_default.xml')
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read("./data/face-trainner.yml")
+    ############################################################################
+
     frame_num = 0
     count = 0
+    name_dic = {}
     # while video is running
     while True:
         return_value, frame = vid.read()
@@ -101,7 +115,7 @@ def main(_argv):
         else:
             print('Video has ended or failed, try a different video format!')
             break
-        frame_num +=1
+        frame_num += 1
         print('Frame #: ', frame_num)
         frame_size = frame.shape[:2]
         image_data = cv2.resize(frame, (input_size, input_size))
@@ -113,7 +127,8 @@ def main(_argv):
         if FLAGS.framework == 'tflite':
             interpreter.set_tensor(input_details[0]['index'], image_data)
             interpreter.invoke()
-            pred = [interpreter.get_tensor(output_details[i]['index']) for i in range(len(output_details))]
+            pred = [interpreter.get_tensor(
+                output_details[i]['index']) for i in range(len(output_details))]
             # run detections using yolov3 if flag is set
             if FLAGS.model == 'yolov3' and FLAGS.tiny == True:
                 boxes, pred_conf = filter_boxes(pred[1], pred[0], score_threshold=0.25,
@@ -159,7 +174,7 @@ def main(_argv):
 
         # by default allow all classes in .names file
         # allowed_classes = list(class_names.values())
-        
+
         # custom allowed classes (uncomment line below to customize tracker for only people)
         allowed_classes = ['person']
 
@@ -176,7 +191,8 @@ def main(_argv):
         names = np.array(names)
         count = len(names)
         if FLAGS.count:
-            cv2.putText(frame, "Objects being tracked: {}".format(count), (5, 35), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 255, 0), 2)
+            cv2.putText(frame, "Objects being tracked: {}".format(
+                count), (5, 35), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 255, 0), 2)
             print("Objects being tracked: {}".format(count))
         # delete detections that are not in allowed_classes
         bboxes = np.delete(bboxes, deleted_indx, axis=0)
@@ -185,9 +201,10 @@ def main(_argv):
         if count > 0:
             # encode yolo detections and feed to tracker
             features = encoder(frame, bboxes)
-            detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(bboxes, scores, names, features)]
+            detections = [Detection(bbox, score, class_name, feature) for bbox,
+                          score, class_name, feature in zip(bboxes, scores, names, features)]
 
-            #initialize color map
+            # initialize color map
             cmap = plt.get_cmap('tab20b')
             colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
 
@@ -195,8 +212,9 @@ def main(_argv):
             boxs = np.array([d.tlwh for d in detections])
             scores = np.array([d.confidence for d in detections])
             classes = np.array([d.class_name for d in detections])
-            indices = preprocessing.non_max_suppression(boxs, classes, nms_max_overlap, scores)
-            detections = [detections[i] for i in indices]       
+            indices = preprocessing.non_max_suppression(
+                boxs, classes, nms_max_overlap, scores)
+            detections = [detections[i] for i in indices]
 
             # Call the tracker
             tracker.predict()
@@ -205,35 +223,72 @@ def main(_argv):
             # update tracks
             for track in tracker.tracks:
                 if not track.is_confirmed() or track.time_since_update > 1:
-                    continue 
+                    continue
                 bbox = track.to_tlbr()
                 class_name = track.get_class()
-                
-            # draw bbox on screen
+                if int(track.track_id) not in name_dic:
+                    name_dic[int(track.track_id)] = str(track.track_id)
+
+                try:
+                    if int(track.track_id) == int(name_dic[int(track.track_id)]):
+                        ############ Face Detection ############
+                        try:
+                            frm = frame.copy()
+                            frm = frm[int(bbox[1]):int(bbox[3]),
+                                      int(bbox[0]):int(bbox[2])]
+                            gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
+                            faces = face_cascade.detectMultiScale(
+                                gray, scaleFactor=1.5, minNeighbors=1)  # Recog. faces
+                            for (x, y, w, h) in faces:
+                                # Convert Face to greyscale
+                                roi_gray = gray[y:y+h, x:x+w]
+
+                                id_, conf = recognizer.predict(
+                                    roi_gray)  # recognize the Face
+                                if conf >= 50:
+                                    name_dic[int(track.track_id)] = str(
+                                        labels[id_])
+                        except Exception as e:
+                            if "Assertion failed" not in str(e):
+                                print(e)
+                                input()
+                except Exception:
+                    pass
+
+                # draw bbox on screen
                 color = colors[int(track.track_id) % len(colors)]
                 color = [i * 255 for i in color]
-                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
-                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
-                cv2.putText(frame, class_name + "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255),2)
+                cv2.rectangle(frame, (int(bbox[0]), int(
+                    bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(
+                    len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
 
-            # if enable info flag then print details about each track
+                # cv2.putText(frame, class_name + "-" + str(track.track_id),
+                #             (int(bbox[0]), int(bbox[1]-10)), 0, 0.75, (255, 255, 255), 2)
+                cv2.putText(frame, str(name_dic[int(track.track_id)]) + "-" + str(track.track_id),
+                            (int(bbox[0]), int(bbox[1]-10)), 0, 0.75, (255, 255, 255), 2)
+
+                # if enable info flag then print details about each track
                 if FLAGS.info:
-                    print("Tracker ID: {}, Class: {},  BBox Coords (xmin, ymin, xmax, ymax): {}".format(str(track.track_id), class_name, (int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3]))))
+                    print("Tracker ID: {}, Class: {},  BBox Coords (xmin, ymin, xmax, ymax): {}".format(
+                        str(track.track_id), class_name, (int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3]))))
 
         # calculate frames per second of running detections
         fps = 1.0 / (time.time() - start_time)
         print("FPS: %.2f" % fps)
         result = np.asarray(frame)
         result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        
+
         if not FLAGS.dont_show:
             cv2.imshow("Output Video", result)
-        
+
         # if output flag is set, save video file
         if FLAGS.output:
             out.write(result)
-        if cv2.waitKey(1) & 0xFF == ord('q'): break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
     cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     try:
